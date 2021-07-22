eac24bab222c1f0f3576676a17d534099a7cf403
Nguyễn Quốc Đạt <73431073+dat-2k2@users.noreply.github.com>
2021-07-22 14:35:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const slalice = require('./slalice.json');
-
+let prefix = slalice.prefix;
 const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
@@ -55,8 +55,6 @@ bot.on('message', message => {
 
 bot.on("message", async message => {
     if(message.author.bot || message.channel.type === "dm") return;
-
-    let prefix = slalice.prefix;
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
