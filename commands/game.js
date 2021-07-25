df77e1e71c2d1853a917cad131d1519f65cb2a2a
Pông <86068524+begaupong2302@users.noreply.github.com>
2021-07-25 09:55:51 +0700

    như cak

--- commands/game.js
+++ commands/game.js
@@ -1,7 +1,7 @@
 ﻿// JavaScript source code
 // JavaScript source code
 module.exports = {
-	name: 'game';
-	description: 'bắt đầu trò chơi';
+	name: 'game',
+	description: 'bắt đầu trò chơi',
 	execute(message) { message.channel.send('hỏi gì đi'); },
 }
